24d5e4e428ba50221639c8e437d442f2cd1dce02
Frederick D <FrederickDelta@gmail.com>
2020-07-26 17:06:09 +0800

    📦 NEW: Bubble sort v2

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,15 +1,29 @@
-const bubbleSort = (list) => {
-  const n = list.length - 1;
+module.exports.v1 = (list) => {
+  let listT = [...list];
+  const n = listT.length - 1;
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (list[j] > list[j + 1]) {
-        let temp = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = temp;
+      if (listT[j] > listT[j + 1]) {
+        let temp = listT[j];
+        listT[j] = listT[j + 1];
+        listT[j + 1] = temp;
       }
     }
   }
-  return list;
+  return listT;
 };
 
-module.exports = bubbleSort;
+module.exports.v2 = (list) => {
+  let listT = [...list];
+  const n = listT.length - 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      if (listT[j] > listT[j + 1]) {
+        let temp = listT[j];
+        listT[j] = listT[j + 1];
+        listT[j + 1] = temp;
+      }
+    }
+  }
+  return listT;
+};
--- sort/bubbleSortTest.js
+++ sort/bubbleSortTest.js
@@ -4,4 +4,5 @@ const bubbleSort = require("./bubbleSort");
 const randomList = new RandomList();
 console.log("Random List:\n", randomList.list);
 console.log("Built-in Sort:\n", randomList.sort());
-console.log("Bubble Sort:\n", bubbleSort(randomList.list));
+console.log("Bubble Sort v1:\n", bubbleSort.v1(randomList.list));
+console.log("Bubble Sort v2:\n", bubbleSort.v2(randomList.list));
