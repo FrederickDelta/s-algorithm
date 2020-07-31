793dd7ceb34ee644e104e1bcef800703029be338
Frederick D <FrederickDelta@gmail.com>
2020-08-01 04:00:12 +0800

    📦 NEW: Bubble sort v3

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -27,3 +27,21 @@ module.exports.v2 = (list) => {
   }
   return listT;
 };
+
+module.exports.v3 = (list) => {
+  let listT = [...list];
+  const n = listT.length - 1;
+  for (let i = 0; i < n; i++) {
+    let sortComplete = true;
+    for (let j = 0; j < n - i; j++) {
+      if (listT[j] > listT[j + 1]) {
+        let temp = listT[j];
+        listT[j] = listT[j + 1];
+        listT[j + 1] = temp;
+        sortComplete = false;
+      }
+    }
+    if (sortComplete) break;
+  }
+  return listT;
+};
--- sort/bubbleSortTest.js
+++ sort/bubbleSortTest.js
@@ -6,3 +6,4 @@ console.log("Random List:\n", randomList.list);
 console.log("Built-in Sort:\n", randomList.sort());
 console.log("Bubble Sort v1:\n", bubbleSort.v1(randomList.list));
 console.log("Bubble Sort v2:\n", bubbleSort.v2(randomList.list));
+console.log("Bubble Sort v3:\n", bubbleSort.v3(randomList.list));
